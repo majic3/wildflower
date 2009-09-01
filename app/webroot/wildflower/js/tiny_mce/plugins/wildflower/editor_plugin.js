713a3e4c9709b17cab198bf20069c90a6d6955bf
majic3 <info@majic3.com>
2009-09-01 03:29:32 +0100

    missed mce updated files

--- app/webroot/wildflower/js/tiny_mce/plugins/wildflower/editor_plugin.js
+++ app/webroot/wildflower/js/tiny_mce/plugins/wildflower/editor_plugin.js
@@ -9,47 +9,16 @@
 			var t = this;
 			
 			// Insert image button
-			ed.addButton('wfinsertimage', {
-				title : 'Insert an image',
-				onclick : function() {
-					 $.jlm.components.tinyMce.insertImage(ed);
-				},
-				'class' : 'mceIcon mce_image'
-			});
+			//ed.addButton('wfinsertimage', {});
 
 			// Insert image button
-			ed.addButton('wfinsertgrid', {
-				title : 'Insert a CSS Based Grid',
-				onclick : function() {
-					 $.jlm.components.tinyMce.insertGrid(ed);
-				},
-				'class' : 'mceIcon'
-			});
+			//ed.addButton('wfinsertgrid', {});
 			
 			// Insert Widget button
-			ed.addButton('wfinsertwidget',  {
-				title : 'Insert a widget',
-				onclick : function() {
-					 $.jlm.components.tinyMce.insertWidget(ed);
-				},
-				'image' : $.jlm.base + '/wildflower/img/cog.png'
-			});
+			//ed.addButton('wfinsertwidget',  {});
 			
 			ed.onDblClick.add(function(ed, e) {
 				// here I want to check if the element is img.widget or img or noneditable block
-				$.jlm.components.widgets.edit(e.target);
-			});
-			
-			// Insert Widget button
-			ed.addButton('wfinserteblock',  {
-				title : 'Insert a Editable Block',
-				onclick : function() {
-					 $.jlm.components.tinyMce.insertEblock(ed);
-				},
-				'image' : $.jlm.base + '/wildflower/img/eblock.png'
-			});
-			
-			ed.onDblClick.add(function(ed, e) {
 				$.jlm.components.widgets.edit(e.target);
 			});
 
@@ -58,34 +27,16 @@
 			//ed.addShortcut('ctrl+l', '??', 'wfinsertwidget');
 
 			// Insert Link button
-			ed.addButton('wfinsertlink',  {
-				title : 'Insert a Link to Content or resource',
-				onclick : function() {
-					var se=ed.selection;
+			//ed.addButton('wfinsertlink',  );
 
-					$.jlm.components.tinyMce.insertLink(ed, se);
-				},
-				'class' : 'mceIcon mce_link'
-				//'image' : $.jlm.base + '/wildflower/img/page_link.png'
-			});
-
-			ed.addButton('wfdivwrapper', {
-				title : 'Wrap Selected Content with a div of choosen class name',
-				image : $.jlm.base + '/wildflower/img/wrap.png',
-				onclick : function()	{
-					var bespokeClass = window.prompt('Please enter the class name of wrapping node');
-					ed.dom.setOuterHTML(ed.selection.getNode(),
-						'<div class="'+bespokeClass+'">'+ed.dom.getOuterHTML(ed.selection.getNode())+'</div>'
-					);
-				},
-	            //'class' : 'mceIcon'
-			});
+			//ed.addButton('wfdivwrapper', );
 		},
 
 		createControl: function(n, cm) {
+			var c = null;
 			switch (n) {
 				case 'wfmedia':
-					var c = cm.createSplitButton('wfinsertimage', {
+					c = cm.createSplitButton('wfmedia', {
 						title : 'Insert Images',
 						'class' : 'mceIcon mce_image'
 					});
@@ -93,9 +44,14 @@
 					c.onRenderMenu.add(function(c, m) {
 						m.add({title : 'Images', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
 
-						m.add({title : 'Insert Image from Wildflower', onclick : function() {
-							alert('Insert Image Clicked.');
-						}});
+						m.add({
+							title : 'Insert Image from Wildflower', 
+							onclick : function() {
+								console.info('Insert Image Clicked.');
+								$.jlm.components.tinyMce.insertImage(ed);
+							}, 
+							'class' : 'mceIcon mce_image'
+						});
 
 						m.add({title : 'Insert Image Stored elsewhere', onclick : function() {
 							alert('Insert External Image Clicked.');
@@ -104,14 +60,12 @@
 						m.add({title : 'Insert swfObject', onclick : function() {
 							alert('Insert swfObject.');
 						}});
-				  });
-
+					});
 				  // Return the new menubutton instance
 				  return c;
-				break;
 
 				case 'wfmodules':
-					var c = cm.createSplitButton('wfmmodules', {
+					c = cm.createSplitButton('wfmodules', {
 						title : 'Manage Modules',
 						'class' : 'mceIcon'
 					});
@@ -119,29 +73,48 @@
 					c.onRenderMenu.add(function(c, m) {
 						m.add({title : 'Wildflower Modules', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
 
-						m.add({title : 'Insert Wildflower Widget', onclick : function() {
-							alert('Insert Wildflower Widget Clicked.');
-						}});
+						m.add({
+							title : 'Insert Wildflower Widget', 
+							image : $.jlm.base + '/wildflower/img/cog.png',
+							onclick : function() {
+								$.jlm.components.tinyMce.insertWidget(ed);
+							}
+						});
 
-						m.add({title : 'Insert Wildflower Menu', onclick : function() {
-							alert('Insert Wildflower Menu Clicked.');
-						}});
+						m.add({
+							title : 'Insert Wildflower Menu', 
+							//image : $.jlm.base + '/wildflower/img/menu.png', 
+							onclick : function() {
+								console.info('Insert Wildflower Menu Clicked.');
+								$.jlm.components.tinyMce.insertMenu(ed);
+							}
+						});
 
-						m.add({title : 'Insert Wildflower Block', onclick : function() {
-							alert('Insert Wildflower Block Clicked.');
-						}});
+						m.add({
+							title : 'Wrap content with a Division with a className of...',
+							image : $.jlm.base + '/wildflower/img/wrap.png',
+							onclick : function()	{
+								//alert('Insert Wildflower Block Clicked.');
+								console.info('Wrap Content Clicked.');
+								var bespokeClass = window.prompt('Please enter the class name of wrapping node');
+								ed.dom.setOuterHTML(ed.selection.getNode(),'<div class="'+bespokeClass+'">'+ed.dom.getOuterHTML(ed.selection.getNode())+'</div>');
+							},
+							//'class' : 'mceIcon'
+						});
 
-						m.add({title : 'Update Embedded Wildflower Blocks', onclick : function() {
-							alert('Update Embedded Wildflower Blocks Clicked.');
-						}});
-				  });
-
+						m.add({
+							title : 'Update Embedded Wildflower Blocks', 
+							//image : $.jlm.base + '/wildflower/img/update.png',
+							onclick : function() {
+								console.info('Update Embedded Wildflower Blocks Clicked.');
+							}
+						});
+					});
 				  // Return the new menubutton instance
 				  return c;
-				break;
 
 				case 'wflinks':
-					var c = cm.createSplitButton('wfinsertlink', {
+					c = cm.createSplitButton('wflinks', {
 						title : 'Links insert, edit or remove',
 						'class' : 'mceIcon mce_link'
 					});
@@ -149,43 +122,35 @@
 					c.onRenderMenu.add(function(c, m) {
 						m.add({title : 'Links', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
 
-						m.add({title : 'Insert Link to Wildflower Page/Post/Category', onclick : function() {
-							alert('Insert Link Clicked.');
-						}});
+						m.add({
+							title : 'Insert a Link to Content or resource',
+							onclick : function() {
+								var se=ed.selection;
+								console.info('Insert Link Clicked.');
+								$.jlm.components.tinyMce.insertLink(ed, se);
+							},
+							'class' : 'mceIcon mce_link'
+							//'image' : $.jlm.base + '/wildflower/img/page_link.png'
+						});
 
-						m.add({title : 'Insert Link to External Thingy', onclick : function() {
-							alert('Insert External Link Clicked.');
-						}});
+						m.add({
+							title : 'Insert Link to Off Site Url', 
+							onclick : function() {
+								var se=ed.selection;
+								console.info('Insert External Link Clicked.');
+								$.jlm.components.tinyMce.insertRemoteLink(ed, se);
+							},
+							'class' : 'mceIcon mce_link'
+						});
 
-						m.add({title : 'Remove Link', onclick : function() {
-							alert('Remove link clicked.');
-						}});
-				  });
-
-				  // Return the new menubutton instance
-				  return c;
-				break;
-
-				case 'wfgrids':
-					var c = cm.createSplitButton('wfcssgrids', {
-						title : 'Insert & Edit CSS Grids',
-						'class' : 'mceIcon'
-					});
-
-					c.onRenderMenu.add(function(c, m) {
-						m.add({title : 'Insert Grids', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
-
-						m.add({title : 'Insert Grid 1', onclick : function() {
-							alert('Insert Grid 1 Clicked.');
-						}});
-
-						m.add({title : 'Insert Grid 2', onclick : function() {
-							alert('Insert Grid 2 Clicked.');
-						}});
-
-						m.add({title : 'Insert Grid 3', onclick : function() {
-							alert('Insert Grid 3 clicked.');
-						}});
+						m.add({
+							title : 'Remove Link', 
+							onclick : function() {
+								var se=ed.selection;
+								alert('Remove link clicked.');
+							},
+							'class' : 'mceIcon mce_unlink'
+						});
 				  });
 
 				  // Return the new menubutton instance
