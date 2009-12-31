7e05e29cb27ab5aea43634e71cbda53827aa41f6
majic3 <info@majic3.com>
2009-12-31 18:15:35 +0000

    klevo wild helper options=null

--- wildflower/views/helpers/wild.php
+++ wildflower/views/helpers/wild.php
@@ -97,7 +97,7 @@ class WildHelper extends AppHelper {
         return $default;
     }
     
-    function menu($slug, $id = false, $class = false) {
+    function menu($slug, $id = false, $class = false, $options = null) {
     	$items = $this->getMenuItems($slug);
     	if (empty($items)) {
     	    return '<p>' . __('Wildflower: There are no menu items for this menu.', true) . '</p>';
