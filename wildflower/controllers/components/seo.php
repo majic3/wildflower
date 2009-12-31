94da3891dcec14e836f395bc6e5dd51e16bea869
majic3 <info@majic3.com>
2009-12-31 22:08:27 +0000

    klevo alterations to seo comp

--- wildflower/controllers/components/seo.php
+++ wildflower/controllers/components/seo.php
@@ -21,30 +21,30 @@ class SeoComponent extends Object {
 	 *
 	 * @param string $pageTitle Title of the current item/page/posts...
 	 */
-    function title($pageTitle = null) {
-        if (!is_object($this->controller)) {
-            return;
-        }
-        
-    	if (!$pageTitle) {
-    	   $pageTitle = $this->controller->pageTitle;
-    	}
-    	if (!$pageTitle) {
-    	   $pageTitle = ucwords($this->controller->params['controller']);
-    	}
-    	
-    	$description = hsc(Configure::read('AppSettings.description'));
-		$keywords = hsc(Configure::read('AppSettings.keywords'));
-    	$nameAndDescription = hsc(Configure::read('AppSettings.site_name'));
-    	if ($description) {
-    	    $nameAndDescription = "$nameAndDescription - {$description}";
-    	}
+	function title($pageTitle = null) {
+		if (!is_object($this->controller)) {
+			return;
+		}
+		
+		if (!$pageTitle) {
+		   $pageTitle = $this->controller->pageTitle;
+		}
+		if (!$pageTitle) {
+		   $pageTitle = ucwords($this->controller->params['controller']);
+		}
+		
+		$description = hsc(Configure::read('Wildflower.settings.description'));
+		$keywords = hsc(Configure::read('Wildflower.settings.keywords'));
+		$nameAndDescription = hsc(Configure::read('Wildflower.settings.site_name'));
+		if ($description) {
+			$nameAndDescription = "$nameAndDescription - {$description}";
+		}
 
 		if ($this->controller->isHome) {
 			$this->controller->pageTitle = $nameAndDescription;
 		} else {
 			$this->controller->pageTitle = "$pageTitle &#8226; $nameAndDescription";
 		}
-    }
+	}
 	
 }
