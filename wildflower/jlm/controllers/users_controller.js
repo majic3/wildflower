a358c1b931bcebeda4221af103a4cc7a043593f9
majic3 <info@majic3.com>
2010-01-04 04:17:10 +0000

    Password strength indicator plugin on users change password - plugin by Benjamin Sterling

--- wildflower/jlm/controllers/users_controller.js
+++ wildflower/jlm/controllers/users_controller.js
@@ -1,5 +1,12 @@
 $.jlm.bind('users.admin_change_password', function() {
 
+	$.fn.passwordStrength.defaults = {
+		classes : Array('is10','is20','is30','is40','is50','is60','is70','is80','is90','is100'),
+		targetDiv : '#passwordStrengthDiv',
+		cache : {}
+	}
+	
+	$('#UserPassword').passwordStrength({targetDiv: '#passwordStrength',classes : Array('is10','is20','is30','is40')});	
     $('form').clearForm();
 	
 });
