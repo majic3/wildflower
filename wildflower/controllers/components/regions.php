7abe2abe67256865215c4212bf31e2a772f42ae6
majic3 <info@majic3.com>
2010-01-11 15:27:50 +0000

    regions comp more

--- wildflower/controllers/components/regions.php
+++ wildflower/controllers/components/regions.php
@@ -35,12 +35,34 @@ class RegionsComponent extends Object {
 			return;
 		}
 
-		//foreach()	{
-			$this->reigonNamesArray[] = 'reigon';
-			$this->controller->set('reigon_for_layout', '<p>my reigon</p>');
-		//}
+		if($options)	{
+			extract($options);
+		}
+
+		foreach($content as $index => $region)	{
+			if($region['region'])	{
+				$flag = true;
+				$this->regionArray[$region['region']][] = $region['content'];
+			} else {
+				$this->regionArray['sidebar'][] = $region['content'];
+			}
+		}
 	}
 	
+	/**
+	 * Set Groups
+	 *
+	 * @param string $pageTitle Title of the current item/page/posts...
+	 */
+	function setGroups() {
+		foreach($this->getNames() as $region => $r)	{
+			$this->controller->set(
+				$r . '_for_layout', 
+				implode('', $this->regionArray[$r])
+			);
+		}
+	}
+
 	/**
 	 * Get Reigon Names
 	 *
