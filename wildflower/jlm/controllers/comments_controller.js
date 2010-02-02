b26efe65ed1f950ebe5766f859ffbcdf4eaa1725
majic3 <info@majic3.com>
2010-02-02 06:35:54 +0000

    edit comments double on li edits comment also (but commeent disappears ? ?)

--- wildflower/jlm/controllers/comments_controller.js
+++ wildflower/jlm/controllers/comments_controller.js
@@ -1,5 +1,10 @@
 $.jlm.bind('comments.admin_index, comments.admin_spam', function () {
     
     $.jlm.components.inplaceEdit.startup();
+
+	// double click list item to edit the item inplace - clicking on para can be difficult
+	$('.comments-list').find('li').live('dblclick', function (event)	{
+		$(this).find('.inplace-edit').trigger('dblclick');
+	});
     
 });
