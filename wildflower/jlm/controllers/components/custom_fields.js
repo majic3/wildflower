8cf7d75105d506d2cce6ea3bf9300a36fb4f25f1
majic3 <info@majic3.com>
2010-02-03 03:17:39 +0000

    custom_fields commented out (needs alot of work)

--- wildflower/jlm/controllers/components/custom_fields.js
+++ wildflower/jlm/controllers/components/custom_fields.js
@@ -7,24 +7,5 @@ $.jlm.addComponent('customFields', {
 	startup: function ()	{
 		this.currentSettingEl = $('.settings', '#currentSettings');
 		this.availSettingEl = $('.settings', '#availableSettings');
-	},
-	add: function ()	{
-		console.info('customFieldsComponent|add');
-	},
-	addCustom: function ()	{
-		console.info('customFieldsComponent|addCustom');
-	},
-	cancel: function ()	{
-		console.info('customFieldsComponent|cancel');
-	},
-	save: function ()	{
-		console.info('customFieldsComponent|save');
-	},
-	delete: function ()	{
-		console.info('customFieldsComponent|delete');
-	},
-	edit: function ()	{
-		console.info('customFieldsComponent|edit');
 	}
-
 });
