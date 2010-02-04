22f8a1f6f3a80d36adfca70bc576eb516970cf23
Sam Bernard <sam@sambernard.net>
2010-02-03 23:59:02 -0600

    Replaced getListThreaded with TreeBehavior::

--- wildflower/controllers/pages_controller.php
+++ wildflower/controllers/pages_controller.php
@@ -118,15 +118,14 @@ class PagesController extends AppController {
 		$this->pageTitle = $this->data[$this->modelClass]['title'];
 		$parentPageOptions = $this->Page->generatetreelist(
 			array(
-				'Page.lft NOT BETWEEN ? AND ?' => array(
-					$this->data['Page']['lft'], $this->data['Page']['rght']
-				),
+				'Page.lft NOT BETWEEN ? AND ?' => array($this->data['Page']['lft'], $this->data['Page']['rght']),
 			), 
 			null, 
 			null, 
 			'-'
 		);
 		$jumpMenu = $this->Page->generatetreelist(null, null, null, ' - ');
+
 		$this->set(compact('parentPageOptions', 'jumpMenu'));
 	}
 
@@ -176,6 +175,7 @@ class PagesController extends AppController {
 		$this->Page->contain('User');
 		$page = $this->Page->findById($this->Page->id);
 		
+
 		// @TODO first check if the page has any children
 		if (Configure::read('Wildflower.settings.home_page_id') != $this->Page->id) { 
 			$this->Page->updateChildPageUrls($this->Page->id, $oldUrl, $page['Page']['url']);
