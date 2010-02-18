cdc6a5acb7886b598ecde7dabb31b6fe981134f5
majic3 <info@majic3.com>
2010-02-18 08:06:25 +0000

    labjs callback class does not cause error (sitemap|swfobject)

---
+++ app/plugins/lab_js/labjs_callback.php
@@ -0,0 +1,14 @@
+<?php
+//*
+class LabjsCallback	 extends object	{
+	public function initialize(&$controller)	{}
+
+	public function beforeFilter(&$controller)	{}
+
+	public function beforeRender(&$controller)	{}
+
+	public function shutdown(&$controller)	{}
+
+	public function beforeRedirect(&$controller, $url, $status = null, $exit = true)	{}
+} //*/
+
