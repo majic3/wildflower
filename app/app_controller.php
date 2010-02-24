db7891e1f8f7709bb105aab19e8d2e00f4fcdd26
majic3 <info@majic3.com>
2010-02-24 09:57:29 +0000

    making category_parent_id into Wildflower.setting.AssetCategory - better than being a prop of app ctrlr; removing credits - thik that should be element

--- app/app_controller.php
+++ app/app_controller.php
@@ -104,7 +104,7 @@ class AppController extends Controller {
 
 		// Home page ID
 		$this->homePageId = intval(Configure::read('AppSettings.home_page_id'));
-		$this->categoryParentId = intval(Configure::read('AppSettings.category_parent_id'));
+		Configure::write('Wildflower.setting.AssetCategory', intval(Configure::read('AppSettings.category_parent_id')));
 
 		// Set cookie defaults
 		$this->cookieName = Configure::read('Wildflower.cookie.name');
@@ -297,9 +297,7 @@ class AppController extends Controller {
 			'bdyClass' => ($this->bdyClass) ? $this->bdyClass : false,
 			'homePageId' => $this->homePageId,
 			// Here without base
-			'here' => substr($this->here, strlen($this->base) - strlen($this->here)),
-			'credits' => $this->isHome ? Configure::read('AppSettings.credits') : false,
-            'categoryParentId' => $this->categoryParentId
+			'here' => substr($this->here, strlen($this->base) - strlen($this->here))
 		);
 		$this->params['Wildflower']['view'] = $params;
 		$this->set($params);
